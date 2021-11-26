15917330fb5b57505e81e3432ff2751dc9076c74
Katya Kostikova <katyakostikova21@gmail.com>
2021-11-26 17:08:25 +0200

    AuthRepo fixed

--- repositories/AuthRepository.js
+++ repositories/AuthRepository.js
@@ -16,6 +16,10 @@ class AuthRepository {
         return await db.query(`select * from passport_authorities`);
     };
 
+    async getDeRegistrators(login) {
+        return await db.query(`select * from persons where (login = '${login}')`);
+    };
+
     async getAdmin(login) {
         return await db.query(`select count (distinct login) from persons where (role = 'Адміністратор' and login = '${login}')`);
     };
@@ -46,7 +50,7 @@ class AuthRepository {
         name, surname, midname, passport_id, birthday, taxnum, login, password, foundation_id, position_id, is_active, role, email
         ) values (
         '${adminData.name.toString()}', '${adminData.surname.toString()}', '${adminData.midname.toString()}', (select id from passports where number = '${adminData.passport_number.toString()}'), '${adminData.birthdate.toString()}', ${adminData.taxnum}, 
-        '${adminData.login.toString()}', '${md5(adminData.password).toString()}', (select id from institutions where name = '${adminData.institution.toString()}'), (select id from positions where name = '${adminData.position.toString()}'), 
+        '${adminData.login.toString()}', '${md5(adminData.password).toString()}', (select id from foundations where name = '${adminData.foundation.toString()}'), (select id from positions where name = '${adminData.position.toString()}'), 
         true, 'Реєстратор', '${adminData.email.toString()}')
         )`
         );
@@ -65,8 +69,43 @@ class AuthRepository {
             }
             return "";
         }
+        else if (type === 2) {
+            let foundAdmins = await this.getUser(data.login.toString());
+            let foundEmails = await this.getEmails(data.email.toString());
+            if (foundAdmins.rows[0].count !== '0') {
+                return "Помилка: адміністратор з таким логіном вже існує";
+            }
+            if (data.password !== data.password_confirmation) return "Помилка: паролі не співпадають";
+            if (foundEmails.rows[0].count === '0') {
+                return "Помилка: Такої пошти Адміністратора не існує";
+            }
+            return "";
+        }
+        else {
+            let foundRegistrators = await this.getUser(data.login.toString());
+            if (foundRegistrators.rows[0].count !== '0') {
+                return "Помилка: Реєстратор з таким логіном вже існує";
+            }
+            if (data.password !== data.password_confirmation) return "Помилка: паролі не співпадають";
+            return "";
+        }
     }
+
+    
+    async getLoginData(loginData) {
+        return await db.query(`select id, role, login from persons where (login = '${loginData.login}' and password = '${md5(loginData.password)}')`);
+    };
+
+    async addCurrentPerson(personId) {
+        return await db.query(`insert into current_per (id) values (${personId})`);
+    };
+
+    async deleteCurrentPerson() {
+        return await db.query(`delete from current_per`);
+    };
 }
 
 
 module.exports = AuthRepository;
+
+
